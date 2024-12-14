71befdd64f57871f23927e01b85e70a3a575f36e
Carlos Paredes <carloss.paredesmorillo@gmail.com>
2024-12-14 10:16:32 +0100

    consult active sessions on authentication

--- Flutter/tv_ratting_app/lib/app/data/repositories_implementation/authentication_repository_implementation.dart
+++ Flutter/tv_ratting_app/lib/app/data/repositories_implementation/authentication_repository_implementation.dart
@@ -1,7 +1,14 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:tv_ratting_app/app/domain/model/user.dart';
 import 'package:tv_ratting_app/app/domain/repositories/authentication_repository.dart';
 
+const _key="sessionId";
 class AuthenticationRepositoryImpl implements AuthenticationRepository{
+  final FlutterSecureStorage _segureStorage;
+
+  AuthenticationRepositoryImpl(this._segureStorage);
+
+  
   @override
   Future<User?> getUserData() {
     return Future.value(User());
@@ -9,7 +16,9 @@ class AuthenticationRepositoryImpl implements AuthenticationRepository{
 
   @override
   
-  Future<bool> get isSignedIn {
-  return Future.value(true);
+  Future<bool> get isSignedIn async{
+  final sessionId = await _segureStorage.read(key: _key);
+  return sessionId!=null;
+  
   }
 }
--- Flutter/tv_ratting_app/lib/main.dart
+++ Flutter/tv_ratting_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:tv_ratting_app/app/data/repositories_implementation/authentication_repository_implementation.dart';
 import 'package:tv_ratting_app/app/data/repositories_implementation/connectivity_repository_impl.dart';
 import 'package:tv_ratting_app/app/data/services/remote/internet_checker.dart';
@@ -13,7 +14,7 @@ void main() {
       Connectivity(),
       InternetChecker(),
     ),
-    authenticationRepository: AuthenticationRepositoryImpl(),
+    authenticationRepository: AuthenticationRepositoryImpl(const FlutterSecureStorage()),
     child: const MyApp(),
   ));
 }
