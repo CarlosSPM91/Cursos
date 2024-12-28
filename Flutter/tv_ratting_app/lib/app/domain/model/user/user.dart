76a0640307b738c6ea74c0767d1ccdccc1c10632
Carlos Paredes <carloss.paredesmorillo@gmail.com>
2024-12-28 10:37:50 +0100

    freezed user

--- Flutter/tv_ratting_app/lib/app/domain/model/user/user.dart
+++ Flutter/tv_ratting_app/lib/app/domain/model/user/user.dart
@@ -1,31 +1,21 @@
-import 'package:equatable/equatable.dart';
-import 'package:json_annotation/json_annotation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 
+part "user.freezed.dart";
 part "user.g.dart";
 
-@JsonSerializable()
-class User extends Equatable {
-  final int id;
-  final String username;
-  @JsonKey(
-    name: "avatar",
-    fromJson: avatarPathFromJson,
-  )
-  final String? avatar_path;
+@freezed
+class User with _$User {
+  const factory User({
+    required int id,
+    required String username,
 
-  const User({
-    required this.id,
-    required this.username,
-    required this.avatar_path,
-  });
-
-  @override
-  List<Object?> get props => [
-        id,
-        username,
-      ];
-
-  Map<String, dynamic> toJson() => _$UserToJson(this);
+    ///
+    @JsonKey(
+      name: "avatar",
+      fromJson: avatarPathFromJson,
+    )
+    String? avatar_path,
+  }) = _User;
 
   factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
 
