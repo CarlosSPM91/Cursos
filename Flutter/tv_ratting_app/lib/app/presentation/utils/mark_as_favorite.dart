dd64567164424325d8701410eab539de65780bf2
Carlos Paredes <carloss.paredesmorillo@gmail.com>
2025-01-20 17:42:55 +0100

    loading dialog

--- Flutter/tv_ratting_app/lib/app/presentation/utils/mark_as_favorite.dart
+++ Flutter/tv_ratting_app/lib/app/presentation/utils/mark_as_favorite.dart
@@ -2,15 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:tv_ratting_app/app/domain/model/media/media.dart';
 import 'package:tv_ratting_app/app/presentation/global/controller/favorites/favorites_controller.dart';
+import 'package:tv_ratting_app/app/presentation/global/dialogs/show_loader.dart';
 
-Future<void> markAsFavorite({
-  required BuildContext context,
-  required Media media,
-  required bool Function() mounted
-}) async {
+Future<void> markAsFavorite(
+    {required BuildContext context, required Media media, required bool Function() mounted}) async {
   final FavoritesController favoritesController = context.read();
-  final result = await favoritesController.markAsFavorite(media);
-  if(!mounted()){
+  final result = await showLoader(
+    context,
+    favoritesController.markAsFavorite(media),
+  );
+  if (!mounted()) {
     return;
   }
   result.whenOrNull(
