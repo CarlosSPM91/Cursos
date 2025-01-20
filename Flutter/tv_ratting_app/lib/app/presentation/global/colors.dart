4662785f10c05bdc29770fbac3f281ff14270cae
Carlos Paredes <carloss.paredesmorillo@gmail.com>
2025-01-20 19:25:48 +0100

    AppTheme

---
+++ Flutter/tv_ratting_app/lib/app/presentation/global/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+AppColors._();
+static const primary = Color(0xff1565c0);
+static const light = Color.fromARGB(255, 203, 235, 229);
+static const dark = Color(0xff000a12);
+static const darkLight = Color(0xff102027);
+
+}
---
+++ Flutter/tv_ratting_app/lib/app/presentation/global/extensions/build_context_extension.dart
@@ -0,0 +1,8 @@
+
+import 'package:flutter/material.dart';
+
+extension BuildContextExt on BuildContext{
+  bool get darkMode{
+    return Theme.of(this).brightness == Brightness.dark;
+  }
+}
