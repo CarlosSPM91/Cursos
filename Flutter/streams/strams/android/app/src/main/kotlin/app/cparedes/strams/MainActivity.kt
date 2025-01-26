3fb2b3459a2a52025e4e8164369d5e4c8b1236d9
Carlos Paredes <carloss.paredesmorillo@gmail.com>
2025-01-26 10:38:58 +0100

    streas first commit

---
+++ Flutter/streams/strams/android/app/src/main/kotlin/app/cparedes/strams/MainActivity.kt
@@ -0,0 +1,5 @@
+package app.cparedes.strams
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ Flutter/streams/strams/lib/app/data/repositories_impl/notification_repository_impl.dart
@@ -0,0 +1,3 @@
+import 'package:strams/app/domain/repositories/notification_repository.dart';
+
+class NotificationRepositoryImpl implements NotificationRepository{ }
