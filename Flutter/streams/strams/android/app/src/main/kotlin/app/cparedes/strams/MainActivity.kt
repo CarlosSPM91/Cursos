023c79473072cc2bd31ed161a981bf0301ca0105
Carlos S. Paredes Morillo <156117845+CarlosSPM91@users.noreply.github.com>
2025-01-26 10:41:03 +0100

    Delete Flutter/streams/strams directory

--- Flutter/streams/strams/android/app/src/main/kotlin/app/cparedes/strams/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package app.cparedes.strams
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- Flutter/streams/strams/lib/app/data/repositories_impl/notification_repository_impl.dart
+++
@@ -1,3 +0,0 @@
-import 'package:strams/app/domain/repositories/notification_repository.dart';
-
-class NotificationRepositoryImpl implements NotificationRepository{ }
